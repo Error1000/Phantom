24cd040eb8e9783d9376755722bdc2aed4e2ba1b
Error1000 <udreadanmihai@gmail.com>
2021-05-24 00:06:47 +0300

    Add all integer computational instructions and a semi-janky JAL implementation

--- README.md
+++ README.md
@@ -2,3 +2,10 @@ Phantom
 -------
 
 A simple RV32E cpu.
+
+NOTE: The AUIPC instruction will
+      just chop off the high 2 bits
+      to convert the native 4-byte address
+      which can cover 16 gb to a byte-oriented
+      address, which means it won't work correctly
+      outside the first 4 gb, LUI will be fine tho.
--- microcode.rs
+++ microcode.rs
@@ -50,41 +50,44 @@ type ControlLine = u32;
     const PC_LOAD: ControlLine      = 1 << 1;
     const PC_OUT: ControlLine       = 1 << 2;
 
-    const ALU_INVA: ControlLine     = 1 << 3;
-    const ALU_INVB: ControlLine     = 1 << 4;
+    const ALU_SWTCHA: ControlLine    = 1 << 3;
+    const ALU_SWTCHB: ControlLine    = 1 << 4;
     const ALU_OP_B2: ControlLine     = 1 << 5;
     const ALU_OP_B1: ControlLine     = 1 << 6;
     const ALU_OP_B0: ControlLine     = 1 << 7;
     
     const ALU_OUTMOD_B1: ControlLine = 1 << 8;
     const ALU_OUTMOD_B0: ControlLine = 1 << 9;
-    const ALU_INVOUT: ControlLine   = 1 << 10;
-    const ALU_OUT: ControlLine      = 1 << 11;
+    const CALC_RET_ADDR: ControlLine = 1 << 10;
+    const ALU_OUT: ControlLine       = 1 << 11;
 
-    const REG_OUT: ControlLine      = 1 << 12;
+    const REG_OUT: ControlLine       = 1 << 12;
 
-    const CNTRL_IMMOUT: ControlLine = 1 << 13;
+    const ALU_MOUT: ControlLine      = 1 << 13;
 
-    const IR_ZR: ControlLine        = 1 << 14;
-    const IR_CEN: ControlLine       = 1 << 15;
-    const IR_LOAD: ControlLine      = 1 << 16;
+    const CNTRL_IMMOUT: ControlLine = 1 << 14;
 
-    const RAM_ENO: ControlLine      = 1 << 17;
-    const RAM_LOAD: ControlLine     = 1 << 18;
-    
+    const IR_ZR: ControlLine        = 1 << 15;
+
+    const RAM_ENO: ControlLine      = 1 << 16;
+    const RAM_LOAD: ControlLine     = 1 << 17;
+
+    const CNTRL_UPIMM: ControlLine  = 1 << 18;
+    const ROUT_TO_RIN: ControlLine  = 1 << 19;
+
     const ALU_OP_ADD:   ControlLine  =         0 |         0 |         0;
     const ALU_OP_AND:   ControlLine  =         0 |         0 | ALU_OP_B0;
     const ALU_OP_XOR:   ControlLine  =         0 | ALU_OP_B1 |         0;
     const ALU_OP_LLSHFT: ControlLine =         0 | ALU_OP_B1 | ALU_OP_B0;
     const ALU_OP_RSHFT: ControlLine  = ALU_OP_B2 |         0 |         0;
     const ALU_OP_SUB:   ControlLine  = ALU_OP_B2 |         0 | ALU_OP_B0;
-    const ALU_OP_6:     ControlLine  = ALU_OP_B2 | ALU_OP_B1 |         0;
+    const ALU_OP_OR:     ControlLine  = ALU_OP_B2 | ALU_OP_B1 |         0;
     const ALU_OP_7:     ControlLine  = ALU_OP_B2 | ALU_OP_B1 | ALU_OP_B0;
 
     const ALU_OUTMOD_NONE: ControlLine    =             0 |             0;
     const ALU_OUTMOD_SIGNBIT: ControlLine =             0 | ALU_OUTMOD_B0;
     const ALU_OUTMOD_SUBC: ControlLine    = ALU_OUTMOD_B1 |             0;
-    const ALU_OUTMOD_3: ControlLine       = ALU_OUTMOD_B1 | ALU_OUTMOD_B0;
+    const ALU_OUTMOD_MUL4: ControlLine    = ALU_OUTMOD_B1 | ALU_OUTMOD_B0;
 
 #[derive(Debug)]
 struct Rom {
@@ -117,32 +120,55 @@ fn main(){
     let mut r = Rom::new();
 
     // LOAD control lines
-    for addr in 0..0b1_000_000_000 {
-        r.data[addr] = PC_ENC | PC_OUT | RAM_ENO | IR_CEN | IR_LOAD
+    for addr in 0 .. 2usize.pow(9) {
+        r.data[addr as usize] |= PC_ENC | PC_OUT | RAM_ENO;
     }
 
     // Integer Register-Immediate Computational Instructions
     const OP_IMM: u8 = 0x04;
-    for ANY in 0u8 ..= 1u8 {
-        r[InstAddr::from_fields_b(0, ANY != 0, OP_IMM)] |= CNTRL_IMMOUT | ALU_OUT | ALU_OUTMOD_NONE    | ALU_OP_ADD;                                     // ADDI
-        r[InstAddr::from_fields_b(1, ANY != 0, OP_IMM)] |= CNTRL_IMMOUT | ALU_OUT | ALU_OUTMOD_NONE    | ALU_OP_LLSHFT;                                  // SLLI  ( shift left logical imm )
-        r[InstAddr::from_fields_b(2, ANY != 0, OP_IMM)] |= CNTRL_IMMOUT | ALU_OUT | ALU_OUTMOD_SIGNBIT | ALU_OP_SUB;                                     // SLTI  ( set less than imm )
-        r[InstAddr::from_fields_b(3, ANY != 0, OP_IMM)] |= CNTRL_IMMOUT | ALU_OUT | ALU_OUTMOD_SUBC    | ALU_OP_SUB;                                     // SLTIU ( set less than imm unsigned )
-        r[InstAddr::from_fields_b(4, ANY != 0, OP_IMM)] |= CNTRL_IMMOUT | ALU_OUT | ALU_OUTMOD_NONE    | ALU_OP_XOR;                                     // XORI
-        r[InstAddr::from_fields_b(5, ANY != 0, OP_IMM)] |= CNTRL_IMMOUT | ALU_OUT | ALU_OUTMOD_NONE    | ALU_OP_RSHFT;                                   // SRLI  ( shift right logical imm )
-        r[InstAddr::from_fields_b(5, ANY != 0, OP_IMM)] |= CNTRL_IMMOUT | ALU_OUT | ALU_OUTMOD_NONE    | ALU_OP_RSHFT;                                   // SRAI  ( shift right arithmetic imm )
-        r[InstAddr::from_fields_b(6, ANY != 0, OP_IMM)] |= CNTRL_IMMOUT | ALU_OUT | ALU_OUTMOD_NONE    | ALU_OP_AND | ALU_INVA | ALU_INVB | ALU_INVOUT;  // ORI
-        r[InstAddr::from_fields_b(7, ANY != 0, OP_IMM)] |= CNTRL_IMMOUT | ALU_OUT | ALU_OUTMOD_NONE    | ALU_OP_AND;                                     // ANDI
+    const OP_LUI: u8 = 0x0D;
+    const OP_AUIPC: u8 = 0x05;
+    const OP_OP:    u8 = 0x0C;
+    const OP_JAL:   u8 = 0x1B;
+    const OP_JALR:  u8 = 0x19;
+
+    for ANY7 in 0u8 ..= 2u8.pow(1)-1 {
+        r[InstAddr::from_fields_b(0, ANY7 != 0, OP_IMM)] |= CNTRL_IMMOUT | ALU_OUT | ALU_OUTMOD_NONE    | ALU_OP_ADD;       // ADDI
+        r[InstAddr::from_fields_b(1, ANY7 != 0, OP_IMM)] |= CNTRL_IMMOUT | ALU_OUT | ALU_OUTMOD_NONE    | ALU_OP_LLSHFT;    // SLLI  ( shift left logical imm )
+        r[InstAddr::from_fields_b(2, ANY7 != 0, OP_IMM)] |= CNTRL_IMMOUT | ALU_OUT | ALU_OUTMOD_SIGNBIT | ALU_OP_SUB;       // SLTI  ( set less than imm )
+        r[InstAddr::from_fields_b(3, ANY7 != 0, OP_IMM)] |= CNTRL_IMMOUT | ALU_OUT | ALU_OUTMOD_SUBC    | ALU_OP_SUB;       // SLTIU ( set less than imm unsigned )
+        r[InstAddr::from_fields_b(4, ANY7 != 0, OP_IMM)] |= CNTRL_IMMOUT | ALU_OUT | ALU_OUTMOD_NONE    | ALU_OP_XOR;       // XORI
+        r[InstAddr::from_fields_b(5, ANY7 != 0, OP_IMM)] |= CNTRL_IMMOUT | ALU_OUT | ALU_OUTMOD_NONE    | ALU_OP_RSHFT;     // SRLI  ( shift right logical imm )
+        r[InstAddr::from_fields_b(5, ANY7 != 0, OP_IMM)] |= CNTRL_IMMOUT | ALU_OUT | ALU_OUTMOD_NONE    | ALU_OP_RSHFT;     // SRAI  ( shift right arithmetic imm )
+        r[InstAddr::from_fields_b(6, ANY7 != 0, OP_IMM)] |= CNTRL_IMMOUT | ALU_OUT | ALU_OUTMOD_NONE    | ALU_OP_OR;        // ORI
+        r[InstAddr::from_fields_b(7, ANY7 != 0, OP_IMM)] |= CNTRL_IMMOUT | ALU_OUT | ALU_OUTMOD_NONE    | ALU_OP_AND;       // ANDI
+
+
+        r[InstAddr::from_fields_b(0, ANY7 != 0, OP_OP)] |= REG_OUT | ALU_OUT | ALU_OUTMOD_NONE    | ALU_OP_ADD;       // ADD
+        r[InstAddr::from_fields_b(1, ANY7 != 0, OP_OP)] |= REG_OUT | ALU_OUT | ALU_OUTMOD_NONE    | ALU_OP_LLSHFT;    // SLL  ( shift left logical )
+        r[InstAddr::from_fields_b(2, ANY7 != 0, OP_OP)] |= REG_OUT | ALU_OUT | ALU_OUTMOD_SIGNBIT | ALU_OP_SUB;       // SLT  ( set less than )
+        r[InstAddr::from_fields_b(3, ANY7 != 0, OP_OP)] |= REG_OUT | ALU_OUT | ALU_OUTMOD_SUBC    | ALU_OP_SUB;       // SLTI ( set less than unsigned )
+        r[InstAddr::from_fields_b(4, ANY7 != 0, OP_OP)] |= REG_OUT | ALU_OUT | ALU_OUTMOD_NONE    | ALU_OP_XOR;       // XOR
+        r[InstAddr::from_fields_b(5, ANY7 != 0, OP_OP)] |= REG_OUT | ALU_OUT | ALU_OUTMOD_NONE    | ALU_OP_RSHFT;     // SRL  ( shift right logical )
+        r[InstAddr::from_fields_b(5, ANY7 != 0, OP_OP)] |= REG_OUT | ALU_OUT | ALU_OUTMOD_NONE    | ALU_OP_RSHFT;     // SRA  ( shift right arithmetic )
+        r[InstAddr::from_fields_b(6, ANY7 != 0, OP_OP)] |= REG_OUT | ALU_OUT | ALU_OUTMOD_NONE    | ALU_OP_OR;        // OR
+        r[InstAddr::from_fields_b(7, ANY7 != 0, OP_OP)] |= REG_OUT | ALU_OUT | ALU_OUTMOD_NONE    | ALU_OP_AND;       // AND
+
+        for ANY3 in 0u8 ..= 2u8.pow(3)-1 {
+            r[InstAddr::from_fields_b(ANY3, ANY7 != 0, OP_LUI)]   |= CNTRL_IMMOUT | CNTRL_UPIMM | ROUT_TO_RIN; // LUI
+            r[InstAddr::from_fields_b(ANY3, ANY7 != 0, OP_AUIPC)] |= CNTRL_IMMOUT | CNTRL_UPIMM | ALU_SWTCHA | ALU_SWTCHB | ALU_OUT | ALU_OUTMOD_MUL4 | ALU_OP_ADD; // AUIPC
+        
+            r[InstAddr::from_fields_b(ANY3, ANY7 != 0, OP_JAL)] = CNTRL_IMMOUT | ALU_SWTCHA | ALU_SWTCHB | ALU_MOUT | ALU_OP_ADD | PC_LOAD | PC_OUT | PC_ENC | IR_ZR | CALC_RET_ADDR; // AUIPC
+        }
     }
 
    // DOWN-CLOCK: SWITCH IRS & LOAD in IR 1 & PARSE in IR 2
    // UP-CLOCK: EXECUTE in IR 2
 
-   // println!("{:?}", r);
     use std::io::Write;
     let mut fout = std::fs::File::create("rom.bin").expect("Opening out file!");
     // Write to file
-    for addr in 0..0b1_000_000_000 {
+    for addr in 0 .. 2usize.pow(9) {
         let b1 = ((r.data[addr] & 0x000000FF) >> 8*0) as u8;
         let b2 = ((r.data[addr] & 0x0000FF00) >> 8*1) as u8;
         let b3 = ((r.data[addr] & 0x00FF0000) >> 8*2) as u8;
